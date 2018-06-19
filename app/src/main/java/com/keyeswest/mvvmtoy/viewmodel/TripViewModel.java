fbcc8b04388728c37cb071ca7e9259cf72772f3d
Chip Keyes <chipk215@msn.com>
2018-06-19 12:15:16 -0600

    trip viewmodel update

--- app/src/main/java/com/keyeswest/mvvmtoy/viewmodel/TripViewModel.java
+++ app/src/main/java/com/keyeswest/mvvmtoy/viewmodel/TripViewModel.java
@@ -3,31 +3,61 @@ package com.keyeswest.mvvmtoy.viewmodel;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 
-import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProvider;
 import android.support.annotation.NonNull;
 
 import com.keyeswest.mvvmtoy.db.entity.TripEntity;
 
 public class TripViewModel  extends AndroidViewModel {
 
-    private final MediatorLiveData<TripEntity> mObservableTrip;
+    private TripEntity mTrip;
 
     //checkbox select state
-    private boolean mSelected;
+    private MutableLiveData<Boolean> mSelected;
 
     public TripViewModel(@NonNull Application application, TripEntity trip){
 
         super(application);
-        mObservableTrip = new MediatorLiveData<>();
-        mObservableTrip.setValue(trip);
+        mTrip = trip;
+
     }
 
-    public void setSelected(){
-        mSelected = true;
+    public void setSelected(Boolean value){
+        mSelected.setValue(value);
     }
 
-    public boolean isSelected(){
+    public MutableLiveData<Boolean> getSelected(){
         return mSelected;
+    }
+
+    public void setTrip(TripEntity trip){
+        this.mTrip = trip;
+    }
+
+    public TripEntity getTrip(){
+        return mTrip;
+    }
+
+
+    public static class Factory extends ViewModelProvider.NewInstanceFactory {
+
+        @NonNull
+        private final Application mApplication;
+        private TripEntity mTrip;
+
+
+        public Factory(@NonNull Application application, TripEntity trip) {
+            mApplication = application;
+            mTrip = trip;
+        }
+
+        @Override
+        public <T extends ViewModel> T create(Class<T> modelClass) {
+            //noinspection unchecked
+            return (T) new TripViewModel(mApplication, mTrip);
+        }
     }
 
 }
